b89edbcc51080dd8b5f27de1b9fe94cfb8e12cfd
Chris Oakes <chris.oakes@outlook.com>
2019-12-13 10:50:39 -0800

    boom done

--- starwars/src/components/Styles.js
+++ starwars/src/components/Styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.div`
 width: 250px
-min-height: 220px;
+min-height: 200px;
 `
 
 export const Entry = styled.div `
@@ -16,10 +16,15 @@ export const BigContainer = styled.div `
 `
 
 export const Button2 = styled.button `
-max-height: 220px;
+height: 200px;
 width:250px;
 margin-top: 24px;
 font-size: 30px;
+background-color: #03cffc;
+border: none;
+box-shadow: 0px 1px 4px rgba(8, 35, 48, 0.2), 0px 2px 15px rgba(8, 35, 48, 0.2);
+border-radius: 30px;
+justify-content: center;
 `
 
 
